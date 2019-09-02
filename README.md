b38d3d2a3a46d040ddbe050263f114673d3ff947
Simon Friis Vindum <simonfv@gmail.com>
2019-09-02 13:59:34 +0200

    Update dependencies and add a readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# PureScript Turbine Starter
+
+Turbine starter kit using PureScrit and pulp.
+
+## Getting started
+
+Clone the repository.
+
+```
+git clone https://github.com/funkia/purescript-turbine-starter my-turbine-project
+```
+
+Enter the project.
+
+```
+cd my-turbine-project
+```
+
+Install the dependencies. This will install both the npm and the Bower
+dependencies.
+
+```
+npm i
+```
+
+Build the code.
+
+```
+npm run build
+```
+
+You can now see the result by opening the `index.html` file in a
+browser.
--- src/Main.purs
+++ src/Main.purs
@@ -3,12 +3,21 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Hareactive.Types (Behavior, Stream, Now)
-import Turbine (Component, runComponent, output, modelView, (</>))
-import Turbine.HTML.Elements as E
+import Hareactive.Types (Behavior)
+import Hareactive.Combinators (accum)
+import Turbine (Component, use, component, output, (</>), runComponent)
+import Turbine.HTML as E
 
-app :: Component {} Unit
-app = E.text "Hello, world!"
+counter :: Component { count :: Behavior Int } {}
+counter = component \on -> do
+  count <- accum (+) 0 (on.increment <> on.decrement)
+  ( E.div { class: pure "wrapper" } (
+      E.text "Counter " </>
+      E.span {} (E.textB $ map show count) </>
+      E.button {} (E.text "+" ) `use` (\o -> { increment: o.click $> 1 }) </>
+      E.button {} (E.text "-" ) `use` (\o -> { decrement: o.click $> -1 })
+    )
+  ) `output` { count }
 
 main :: Effect Unit
-main = runComponent "#mount" app
+main = runComponent "#mount" counter
